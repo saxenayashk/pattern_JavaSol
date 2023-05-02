c0c60a00a129205b986489942e4938d07f057b5e
Yash Saxena <66787500+saxenayashk@users.noreply.github.com>
2023-05-02 19:03:17 +0530

    Add files via upload

--- AdvancePatterns.java
+++ AdvancePatterns.java
@@ -72,6 +72,161 @@ public class AdvancePatterns {
             i++;
         }
     }
+    public static void leftTriangleAlphaPatt2(int n)
+    {
+        int i=1;
+        while(i<=n)
+        {
+            char ch=(char)('A'+n-i);
+            int j=1;
+            while(j<=i)
+            {
+                System.out.print(ch+" ");
+                ch++;
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    public static void rightTriangleStarPatt(int n)
+    {
+        int i=1;
+        while(i<=n)
+        {
+            int j=1;
+            while(j<=n-i)
+            {
+                System.out.print(" ");
+                j++;
+            }
+            int k=1;
+            while(k<=i)
+            {
+                System.out.print("*");
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    public static void rightTriangleStarPatt2(int n)
+    {
+        int i=1;
+        while(i<=n)
+        {
+            int j=1;
+            while(j<=n-i+1)
+            {
+              System.out.print("*");
+              j++;
+            }
+            int k=1;
+            while(k<=i-1)
+            {
+                System.out.print(" ");
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    public static void rightTriangleStarPatt3(int n)
+    {
+        int i=1;
+        while(i<=n)
+        {
+            int j=1;
+            while(j<=i-1)
+            {
+                System.out.print(" ");
+                j++;
+            }
+            int k=1;
+            while(k<=n-i+1)
+            {
+                System.out.print("*");
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    public static void rightTriangleNumPatt(int n)
+    {
+        int i=1;
+        while(i<=n)
+        {
+            int j=1;
+            while(j<=i-1)
+            {
+                System.out.print(" ");
+                j++;
+            }
+            int k=1;
+            while(k<=n-i+1)
+            {
+                System.out.print(i);
+                k++;
+            }
+            i++;
+            System.out.println();
+        }
+    }
+    public static void rightTriangleNumPatt2(int n)
+    {
+        int i=1;
+        int x=1;
+        while(i<=n)
+        {
+            int j=1;
+            while(j<=n-i)
+            {
+                System.out.print("  ");
+                j++;
+            }
+            int k=1;
+            while(k<=i)
+            {
+                System.out.print(x+" ");
+                x++;
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    public static void pyramidNumPattern(int n)
+    {
+        int i=1;
+        while(i<=n)
+        {
+            int j=1;
+            while(j<=n-i)
+            {
+                System.out.print("  ");
+                j++;
+            }
+            int k=1;
+            while(k<=i)
+            {
+                System.out.print(k+" ");
+                k++;
+            }
+            int l=2;
+            while(l<=i)
+            {
+                int res=l-1;
+                if(res>0)
+                {
+                    System.out.print(res+" ");
+                }
+                l++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
     public static void main(String[] args) {
        alphabetSquareRow(5); 
        System.out.println("----------------"); 
@@ -80,6 +235,21 @@ public class AdvancePatterns {
        alphabetSquarePatt2(5);
        System.out.println("-------------------"); 
        leftTriangleAlphaPatt(5);
+       System.out.println("-------------------"); 
+       leftTriangleAlphaPatt2(5);
+       System.out.println("-------------------"); 
+       rightTriangleStarPatt(5);
+       System.out.println("-------------------"); 
+       rightTriangleStarPatt2(5);
+       System.out.println("---------------------");
+       rightTriangleStarPatt3(5);
+       System.out.println("------------------------");
+       rightTriangleNumPatt(5);
+       System.out.println("------------------------");
+       rightTriangleNumPatt2(5);
+       System.out.println("------------------------");
+       pyramidNumPattern(5);
+
     }
     /*
      OUTPUT:-
@@ -108,5 +278,54 @@ public class AdvancePatterns {
      D E F 
      G H I J 
      K L M N O 
+
+     -------------------
+     E 
+     D E 
+     C D E 
+     B C D E 
+     A B C D E 
+
+     -------------------
+       *
+      **
+     ***
+    ****
+   *****
+
+    -------------------
+    *****
+    **** 
+    ***  
+    **   
+    *   
+
+    ---------------------
+    *****
+     ****
+      ***
+       **
+        *
+
+    ------------------------
+    11111
+     2222
+      333
+       44
+        5    
+
+    ------------------------
+        1 
+      2 3 
+    4 5 6 
+  7 8 9 10 
+11 12 13 14 15 
+
+    ------------------------
+        1 
+      1 2 1 
+    1 2 3 1 2 
+  1 2 3 4 1 2 3 
+1 2 3 4 5 1 2 3 4 
      */
 }
